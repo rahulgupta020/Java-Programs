922887a4901077b5a31cb7710f8d9aefb091a937
Rahul Gupta <42673573+rahulzxcvbn@users.noreply.github.com>
2020-11-10 12:17:37 +0530

    Update and rename 1a.java to Program1a.java

---
+++ Program1a.java
@@ -0,0 +1,8 @@
+/*
+Write a Java program that takes a number as input and prints its multiplication table upto 10
+
+Save File as Program1a.java
+Compile :- javac Program1a.java
+Run :-     java Program1a
+
+*/
