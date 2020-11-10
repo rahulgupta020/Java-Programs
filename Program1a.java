da4fa274a4f83fdc9665bb118d3b8d167e8acd82
Rahul Gupta <42673573+rahulzxcvbn@users.noreply.github.com>
2020-11-10 12:45:18 +0530

    Update Program1a.java

--- Program1a.java
+++ Program1a.java
@@ -6,3 +6,20 @@ Compile :- javac Program1a.java
 Run :-     java Program1a
 
 */
+
+import java.util.*;
+class Program1a
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n;
+		System.out.print("Enter a number :- ");
+		n = sc.nextInt();
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n + " * "+ i + " = "+ n*i );
+		}
+	}
+};
