f5798ce08377fcae657ded14708877fde28f4151
Rahul Gupta <42673573+rahulzxcvbn@users.noreply.github.com>
2020-11-10 12:29:07 +0530

    Update Program1a.java

--- Program1a.java
+++ Program1a.java
@@ -1,7 +1,7 @@
 /*
 Write a Java program that takes a number as input and prints its multiplication table upto 10
 
-Save File as Program1a.java
+Save File as <b>Program1a.java</b>
 Compile :- javac Program1a.java
 Run :-     java Program1a
 
